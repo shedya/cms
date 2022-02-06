c2ab9ec469a4c1efa5dc42c2bd18070d048239f9
SHED <53966497+shedya@users.noreply.github.com>
2022-02-07 04:59:52 +0800

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <link rel="alternate icon" class="js-site-favicon" type="image/png" href="https://s4.ax1x.com/2022/02/07/HMP7VI.png">
     </Head>
   );
 }
